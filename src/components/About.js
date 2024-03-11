45bf050d2bfd75378a442215f90c48b3b09efe9a
varshasharma01 <varshasinbox1@gmail.com>
2024-03-11 14:40:48 +0530

    Updated code

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,7 @@ import { useSpring, animated } from "react-spring";
 import SimpleCarousel from "simple-react-carousel";
 
 const teamMembers = [
-  {
-    name: "Utkarsh",
-    photo:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQEnrE8osXZM4Q/profile-displayphoto-shrink_400_400/0/1568148063945?e=1625702400&v=beta&t=02WqZMxtw2EI8B0_mmMv22_Oi2S4RB_c74tNWrl0GTA",
-    description:
-      "Pursuing B.Tech in Computer Science from BK Birla Institute Of Engineering And Technology. He is a passionate coder and loves to work in a team. He is a quick learner and loves to learn new technologies.",
-    gitHub: "",
-    linkedIcon: "https://www.linkedin.com/in/isabelmehlmann/",
-  },
+
   {
     name: "Varsha Sharma",
     photo:
@@ -21,6 +13,35 @@ const teamMembers = [
       "Final year BCA student at Chandigarh University. DSA enthusiast, passionate about coding and learning new technologies. She is a quick learner and loves to work in a team.",
     gitHub: "",
     linkedIcon: "https://www.linkedin.com/in/varsha-sharma-41004b228/",
+  },
+
+  {
+    name: "Yash Vijay",
+    photo:
+      "",
+    description:
+      "Final year BCA student at Chandigarh University. DSA enthusiast, passionate about coding and learning new technologies. She is a quick learner and loves to work in a team.",
+    gitHub: "",
+    linkedIcon: "https://www.linkedin.com/in/varsha-sharma-41004b228/",
+  },
+
+  {
+    name: "Rohan",
+    photo:
+      "",
+    description:
+      "Final year BCA student at Chandigarh University. DSA enthusiast, passionate about coding and learning new technologies. She is a quick learner and loves to work in a team.",
+    gitHub: "",
+    linkedIcon: "https://www.linkedin.com/in/varsha-sharma-41004b228/",
+  },
+  {
+    name: "Dhruv Dhingra",
+    photo:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQEnrE8osXZM4Q/profile-displayphoto-shrink_400_400/0/1568148063945?e=1625702400&v=beta&t=02WqZMxtw2EI8B0_mmMv22_Oi2S4RB_c74tNWrl0GTA",
+    description:
+      "Pursuing B.Tech in Computer Science from BK Birla Institute Of Engineering And Technology. He is a passionate coder and loves to work in a team. He is a quick learner and loves to learn new technologies.",
+    gitHub: "",
+    linkedIcon: "https://www.linkedin.com/in/isabelmehlmann/",
   },
 
 ];
